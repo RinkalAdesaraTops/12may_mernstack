927078ee999473f58904a8735a287e81051aeb0d
Rinkal <rinkaladesara.tops@gmail.com>
2025-11-06 15:05:07 +0530

    crud

--- nodejs/project/controllers/CatController.js
+++ nodejs/project/controllers/CatController.js
@@ -1,21 +1,51 @@
+const { ObjectId } = require('mongodb');
 const main = require('../models/CatModel')
 const ins = async(req,res)=>{
     //insert 
     const conn = await main()
     let db = conn.db
     const collection = db.collection('category');
+
     let result = await collection.insertOne(req.body)
     if(result){
         res.redirect('/category')
     }
-
 }
 
-const del = (req,res)=>{
-    //delete 
-}
-const disp = (req,res)=>{
-    res.render('category')
+const del = async(req,res)=>{
+    let catid = req.params.id
+    let id = new ObjectId(catid)
+    const conn = await main()
+    let db = conn.db
+    const collection = db.collection('category');
+    let result = await collection.deleteOne({_id:id})
+    if(result){
+        res.redirect('/category')
+    }
 }
 
-module.exports = {ins,del,disp}
+const edit = async(req,res)=>{
+    let catid = req.params.id
+    let id = new ObjectId(catid)
+    const conn = await main()
+    let db = conn.db
+    const collection = db.collection('category');
+    let editdata = await collection.findOne({_id:id})
+    let data = await collection.find().toArray()
+    res.render('category',{
+        "catdata":data,
+        "editData":editdata
+    })
+}
+const disp = async(req,res)=>{
+    const conn = await main()
+    let db = conn.db
+    const collection = db.collection('category');
+    let data = await collection.find().toArray()
+    res.render('category',{
+        "catdata":data,
+        "editData":""
+    })
+}
+
+module.exports = {ins,del,disp,edit}
--- nodejs/project/routes/CatRoutes.js
+++ nodejs/project/routes/CatRoutes.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var router = express.Router()
-const {disp,ins,del}= require('../controllers/CatController')
+const {disp,ins,del,edit}= require('../controllers/CatController')
 router.get('/',disp)
 router.post('/insdata',ins)
+router.get('/del/:id',del)
+router.get('/edit/:id',edit)
 
 module.exports = router
